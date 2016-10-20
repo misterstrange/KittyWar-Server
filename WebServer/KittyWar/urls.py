597e2eab8a18df59433aaad6297c5c29205c79ac
Juan Gonzalez <jgonzalez498@gmail.com>
2016-10-20 07:43:46 -0700

    Added url /Kittywar/register/mobile/ to accept registration through post json

--- WebServer/KittyWar/urls.py
+++ WebServer/KittyWar/urls.py
@@ -5,11 +5,12 @@ from . import views
 app_name = 'kittywar'
 urlpatterns = [
 
-    url(r'^$',                  views.index_view,        name = 'index'),
-    url(r'Kittywar/register/$', views.register_view,     name = 'register'),
-    url(r'Kittywar/login/$',    views.login_view,        name = 'login'),
-    url(r'Kittywar/logout/$',   views.logout_view,       name = 'logout'),
-    url(r'Kittywar/home/$',     views.home_view,         name = 'home'),	
+    url(r'^$',                         views.index_view,           name='index'),
+    url(r'Kittywar/register/$',        views.register_view,        name='register'),
+    url(r'Kittywar/register/mobile/$', views.register_mobile_view, name='register_mobile'),
+    url(r'Kittywar/login/$',           views.login_view,           name='login'),
+    url(r'Kittywar/logout/$',          views.logout_view,          name='logout'),
+    url(r'Kittywar/home/$',            views.home_view,            name='home'),
 ]
 
 

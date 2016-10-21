8879445c06050ede7869f112a337049ad28e5e8f
1moosey1 <1moosey1@users.noreply.github.com>
2016-10-20 20:38:10 -0700

    Update models.py

--- WebServer/KittyWar/models.py
+++ WebServer/KittyWar/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 
 class CatCard(models.Model):
+    
     breed = models.CharField(max_length=30)
     health = models.PositiveIntegerField(default=10)
-    AbAttribute = models.CharField(max_length=30)
-    AbText = models.CharField(max_length=100)
+    attribute_id = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
     flavor = models.CharField(max_length=100)
 
 
 class BasicCards(models.Model):
-    Title = models.CharField(max_length=10)
-    attribute = models.CharField(max_length=10)
-    AbAttribute = models.CharField(max_length=10)
-    AbText = models.CharField(max_length=100)
+    
+    title = models.CharField(max_length=10)
+    attribute_id = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
     flavor = models.CharField(max_length=100)
